14ac0e7573c6162c4b445f4674a70a9038999531
noseunghyeon <ro981009@gmail.com>
2024-10-31 18:20:19 +0900

    new

---
+++ src/components/Features/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="hero-wrapper flex flex-col items-center mt-6">
+      <div className="slogan lg:text-7xl sm:text-6xl text-4xl text-center dark:text-neutral-300 tracking-wide text-neutral-700">
+        <h2 className="bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+          Information
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
+import Test from "./Test";
 
 const index = () => {
   return (
@@ -9,6 +10,7 @@ const index = () => {
       <div className="container">
         <Header />
         <Body />
+        <Test />
         <Footer />
       </div>
     </div>
