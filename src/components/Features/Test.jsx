4a2ed7ef55198de9656808495d4738604a3cc7c7
noseunghyeon <ro981009@gmail.com>
2024-11-01 10:46:10 +0900

    new

--- src/components/Features/Test.jsx
+++ src/components/Features/Test.jsx
@@ -2,11 +2,97 @@ import React from "react";
 
 const Test = () => {
   return (
-    <div className="hero-wrapper flex flex-col items-center mt-6">
+    <div className="proverbs hero-wrapper flex flex-col items-center mt-6 ">
       <div className="slogan lg:text-7xl sm:text-6xl text-4xl text-center dark:text-neutral-300 tracking-wide text-neutral-700">
-        <h2 className="bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
-          Information
+        <h2 className=" bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+          proverbs
         </h2>
+      </div>
+      <div className="cage flex flex-wrap justify-center">
+        <div className="table_1 flex mt-10 text-center">
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              You will face many defeats in life, but never let yourself be
+              defeated.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Maya Angelou
+              </div>
+            </div>
+          </div>
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              The greatest glory in living lies not in never falling, but in
+              rising every time we fall.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Nelson Mandela
+              </div>
+            </div>
+          </div>
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              In the end, it’s not the years in your life that count. It’s the
+              life in your years.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Abraham Lincoln
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="table_2 flex mt-10 text-center">
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              Life is either a daring adventure or nothing at all.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Helen Keller
+              </div>
+            </div>
+          </div>
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              Many of life’s failures are people who did not realize how close
+              they were to success when they gave up.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Thomas A. Edison
+              </div>
+            </div>
+          </div>
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text ">
+              Keep your eyes on the stars and your feet on the ground.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Theodore Roosevelt
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="table_3 flex mt-10 text-center">
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              Try not to become a man of success but rather try to become a man
+              of value.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                Albert Einstein
+              </div>
+            </div>
+          </div>
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              Don’t be afraid to give up the good to go for the great.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                John D. Rockefeller
+              </div>
+            </div>
+          </div>
+
+          <div className="dark:bg-neutral-900 light: bg-white rounded-md p-6 text-md border border-neutral-800 dark:font-thin light: font-normal dark:text-neutral-300 light: text-black mx-1">
+            <div className="text">
+              Do not be afraid to give up the good to go for the great.
+              <div className="name my-4 bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
+                John D. Rockefeller
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
